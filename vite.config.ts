232595780eb7f543e66f468f108df1ae0a35d00c
Miréio Trinley <mireiotrinley@icloud.com>
2025-12-01 12:13:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [react()],
   // GitHub Pages configuration
-  base: process.env.NODE_ENV === 'production' ? '/Webpage/' : '/',
+  base: '/Webpage/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
